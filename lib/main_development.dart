7bd5ae0fcf926a05a5a089cfd8a1fc2ac6756539
Abdo010101 <abdelmenem.developer@gmail.com>
2024-08-25 16:30:32 +0300

    added lauch.json for andorid flavors config and add flavors for development and  production flavors changes android app name for each flavors added two main file

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,9 +3,13 @@ import 'package:development/core/routing/app_router.dart';
 import 'package:development/doc_app.dart';
 import 'package:development/features/login/ui/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setUp();
+  // to fix text being hidden  bug in screen util in realse mode
+
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
